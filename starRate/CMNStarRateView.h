86da9d35d03aa071510a87db0982dade1a7ea8b8
Tao Peng <taopeng@ZHs-MacBook-Air.local>
2015-04-23 17:43:38 +0800

    first commit

---
+++ starRate/CMNStarRateView.h
@@ -0,0 +1,13 @@
+//
+//  CMNStarRateView.h
+//  starRate
+//
+//  Created by Tao Peng on 15/4/23.
+//  Copyright (c) 2015年 Tao Peng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CMNStarRateView : UIView
+
+@end
---
+++ starRate/CMNStarRateView.m
@@ -0,0 +1,21 @@
+//
+//  CMNStarRateView.m
+//  starRate
+//
+//  Created by Tao Peng on 15/4/23.
+//  Copyright (c) 2015年 Tao Peng. All rights reserved.
+//
+
+#import "CMNStarRateView.h"
+
+@implementation CMNStarRateView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
